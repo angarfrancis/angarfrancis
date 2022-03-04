09d1aef3d5df7bbe047963b660180daf6f87e12c
angarfrancis <angarfrancis68@gmail.com>
2022-03-04 01:57:27 -0800

    sonmething to added

--- calculator_test.py
+++ calculator_test.py
@@ -8,7 +8,23 @@ class Test(unittest.TestCase):
     def test_0_add(self):
         result = self.calculator.add(4,8)
         self.assertEqual(result,12)
-if __name__ == '__main__':
-    # begin the unittest.main()
-    unittest.main()
     
+    def test_0_subtract(self):
+        result = self.calculator.subtract(9,5)
+        self.assertEqual(result,4)
+
+    def test_0_Multiply(self):
+        result = self.calculator.Multiply(2,3)
+        self.assertEqual(result,6)
+
+    def test_0_Divide(self):
+        result = self.calculator.Divide(6,3)
+        self.assertEqual(result,2)
+
+    def test_0_Power(self):
+        result = self.calculator.Power(2,2)
+        self.assertEqual(result,4)
+
+        if __name__ == '__main__':
+    # begin the unittest.main()
+         unittest.main()
